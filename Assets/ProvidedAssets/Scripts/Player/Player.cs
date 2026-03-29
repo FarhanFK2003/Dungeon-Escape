3ba7a9461dbe68f6a0ba44ce51973b7eba75ad87
FarhanFK2003 <farhan.khanfk2003@gmail.com>
2026-03-30 00:56:32 +0500

    Added the Assets

---
+++ Assets/ProvidedAssets/Scripts/Player/Player.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _jumpForce = 5.0f;
+    [SerializeField] private float _speed = 3.0f;
+    // [SerializeField] private LayerMask _groundLayer;
+
+    private Rigidbody2D _rigid;
+    private PlayerAnimation _playerAnimation;
+    private SpriteRenderer _playerSpriteRenderer;
+    private SpriteRenderer _swordSpriteRenderer;
+    private bool _resetJump = false;
+    private bool _grounded = false;
+   
+
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+        _playerAnimation = GetComponent<PlayerAnimation>();
+        _playerSpriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        _swordSpriteRenderer = transform.GetChild(1).GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        PlayerMovement();
+
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded() == true)
+        {
+            PlayerJump();
+            StartCoroutine(ResetJumpRoutine());
+        }
+
+        if(Input.GetMouseButtonDown(0) && IsGrounded() == true)
+        {
+            _playerAnimation.Attack();
+        }  
+    }
+
+    private void PlayerMovement()
+    {
+        // GetAxisRaw gives us only -1, 0 and 1
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+        _grounded = IsGrounded();
+
+        _rigid.linearVelocity = new Vector2(horizontalMove * _speed, _rigid.linearVelocity.y);
+
+        if(horizontalMove > 0)
+        {
+            Flip(true);
+        }
+        else if(horizontalMove < 0)
+        {
+            Flip(false);
+        }
+
+        _playerAnimation.Move(horizontalMove);
+    }
+
+    private void PlayerJump()
+    {
+        _rigid.linearVelocity = new Vector2(_rigid.linearVelocity.x, _jumpForce);
+
+        _playerAnimation.Jump(true);
+
+    }
+
+    bool IsGrounded()
+    {
+        // Vector2 origin, Vector2 direction, float distance, int layerMask
+        // We can use layer value or bit operator.
+        // RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, _groundLayer.value);
+        // The << operator shifts its left-hand operand left by the number of bits defined by its right-hand operand.        
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 1f, 1 << 8);
+        // Debug.DrawRay(transform.position, Vector2.down * 0.6f, Color.green);
+
+        if(hitInfo.collider != null)
+        {
+            if(_resetJump == false)
+            {
+                //Debug.Log("Hit: " + hitInfo.collider.name);
+                _playerAnimation.Jump(false);
+                return true;   
+            }                     
+        }
+
+        return false;
+    }
+
+    IEnumerator ResetJumpRoutine()
+    {
+        _resetJump = true;
+        yield return new WaitForSeconds(0.1f);
+        _resetJump = false;
+    }
+
+    void Flip(bool faceRight)
+    {
+        if(faceRight)
+        {
+            _playerSpriteRenderer.flipX = false;
+            _swordSpriteRenderer.flipX = false;
+            _swordSpriteRenderer.flipY = false;
+
+            Vector3 newPosition = _swordSpriteRenderer.transform.localPosition;
+            newPosition.x = 1.01f;
+            _swordSpriteRenderer.transform.localPosition = newPosition;
+        }
+        else if(!faceRight)
+        {
+            _playerSpriteRenderer.flipX = true;
+            _swordSpriteRenderer.flipX = true;
+            _swordSpriteRenderer.flipY = true;
+
+            Vector3 newPosition = _swordSpriteRenderer.transform.localPosition;
+            newPosition.x = -1.01f;
+            _swordSpriteRenderer.transform.localPosition = newPosition;
+        }
+    }
+}
